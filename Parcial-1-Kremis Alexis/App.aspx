924fa0516e2ea9785724447713a58aa734c21e00
akremis <alexisivan.kremisarroyo.contractor@gdevtools.com>
2024-10-09 17:54:09 -0300

    Agregar archivos de proyecto.

---
+++ Parcial-1-Kremis Alexis/App.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="App.aspx.cs" Inherits="Parcial_1_Kremis_Alexis.App" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- Campo para ingresar el nombre del sector -->
+            <asp:TextBox ID="TextBox1" runat="server" placeholder="Ingrese el nombre del sector"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Filtrar" OnClick="Button1_Click" />
+            
+            <!-- GridView para mostrar la lista de empleados -->
+            <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="id" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" ShowSelectButton="True" />
+                    <asp:BoundField DataField="id" HeaderText="id" InsertVisible="False" ReadOnly="True" SortExpression="id" />
+                    <asp:BoundField DataField="apellido" HeaderText="apellido" SortExpression="apellido" />
+                    <asp:BoundField DataField="nombre" HeaderText="nombre" SortExpression="nombre" />
+                </Columns>
+            </asp:GridView>
+            
+            <!-- SqlDataSource que obtiene la lista de empleados -->
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+                ConnectionString="<%$ ConnectionStrings:IssdTP42023ConnectionString %>" 
+                SelectCommand="SELECT * FROM [Empleados]">
+            </asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Parcial-1-Kremis Alexis/App.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace Parcial_1_Kremis_Alexis
+{
+    public partial class App : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Console.WriteLine("Execute consult SQL SUCCESS ");
+                SqlDataSource1.SelectCommand = "SELECT * FROM [Empleados]";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBox1.Text))
+            {
+                int idSector = ObtenerIdSector(TextBox1.Text);
+
+                if (idSector > 0)
+                {
+                    SqlDataSource1.SelectCommand = "SELECT * FROM [Empleados] WHERE [id_sector] = @id_sector";
+                    SqlDataSource1.SelectParameters.Clear();
+                    SqlDataSource1.SelectParameters.Add("id_sector", idSector.ToString());
+                }
+                else
+                {
+                    SqlDataSource1.SelectCommand = "SELECT * FROM [Empleados]";
+                }
+            }
+            else
+            {
+                SqlDataSource1.SelectCommand = "SELECT * FROM [Empleados]";
+            }
+
+            GridView1.DataBind();
+            Console.WriteLine("Execute consult SQL SUCCESS ");
+        }
+
+        private int ObtenerIdSector(string nombreSector)
+        {
+            Console.WriteLine("Execute ObtenerIdSector" + ": " + nombreSector);
+            int idSector = -1; 
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["IssdTP42023ConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT id FROM EmpleadoSectores WHERE nombre = @nombreSector";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@nombreSector", nombreSector);
+
+                try
+                {
+                    conn.Open();
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        Console.WriteLine("Execute SQL SUCCESS " + ": " + result);
+                        idSector = Convert.ToInt32(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return idSector;
+        }
+    }
+}
